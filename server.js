d896f40acca0b597dfe326b482d79daad9d63b27
Kristoffer Stobbe <krisstobbe@yahoo.com>
2019-04-11 17:47:40 -0500

    Adding Upload Feature
    User must upload document, and them submit form

--- server.js
+++ server.js
@@ -19,6 +19,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const app = express();
+const nodemailer = require("nodemailer");
+var http = require('http');
+var formidable = require('formidable');
+var fs = require('fs');
 app.use(bodyParser.urlencoded({extended: true}))
 
 /*********** Setting the Port to Listen To ***********/
@@ -32,16 +36,64 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
   console.log('Arrived at Index Home')
+
 })
+
+app.post('/upload', function (req, res){
+    var form = new formidable.IncomingForm();
+    form.parse(req);
+    form.on('fileBegin', function (name, file){
+        file.path = __dirname + '/uploads/' + file.name;
+    });
+    form.on('file', function (name, file){
+        console.log('Uploaded ' + file.name);
+    });
+    res.redirect('/')
+});
+
 
 app.post('/informationRequest', (req, res) => {
-    db.collection('userinfo').save(req.body, (err, result) => {
-    if (err) return console.log(err)
 
-    console.log('saved to database')
+    db.collection('userinfo-new').save(req.body, (err, result) => {
+    if (err) return console.log(err)
+    console.log('Saved to database')
     res.redirect('/')
+
+
+    /****************************************************/
+    async function main(){
+      // Generate test SMTP service account from Gmail Email
+      let testAccount = await nodemailer.createTestAccount();
+
+      // create reusable transporter object using the default SMTP transport
+      let transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: 'saslogcollector@gmail.com',
+            pass: 'test01**test'
+        }
+      });
+
+      // send mail with defined transport object
+      let info = await transporter.sendMail({
+        from: '"SAS Log Collector" <saslogcollector@gmail.com>', // sender address
+        to: "saslogcollector@gmail.com", // list of receivers
+        subject: "Track 76198475 SDW Logs", // Subject line
+        attachments: {path: 'logs/76198475_SDW.txt'},
+        text: "", // plain text body
+        html: "<b>The attachment is for 76198475. It contains SDW logs.</b>" // html body
+      });
+      console.log("Message sent: %s", info.messageId);
+      // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+    }
+    main().catch(console.error);
+    /*******************************************************************************/
+
   })
 })
+
 
 /* Ideas on how to implement */
 /* 
