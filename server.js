b839542e4bcf3edc80443942930218b957f4db69
Kristoffer Stobbe <krisstobbe@yahoo.com>
2019-04-15 16:34:00 -0500

    File Update is Connected
     let custTrackingNumber = result[0].custTrackingNumber;
    
    This was what I was working on. It was an object inside an array.

--- server.js
+++ server.js
@@ -17,16 +17,11 @@ MongoClient.connect(process.env.DATABASE, (err, client) => {
 /*********** Requirements and Server Setup ***********/
 const express = require('express');
 const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt-nodejs');
 const app = express();
 const nodemailer = require("nodemailer");
-const busboyBodyParser = require('busboy-body-parser');
-var http = require('http');
 var formidable = require('formidable');
 var fs = require('fs');
 app.use(bodyParser.urlencoded({extended: true}));
-//parse multipart/form-data    
-// app.use(busboyBodyParser());
 
 /*********** Setting the Port to Listen To ***********/
 app.listen(3000, function() {
@@ -35,13 +30,79 @@ app.listen(3000, function() {
 
 /*********** Using Static Files like CSS/JS to Display with Response ***********/
 app.use(express.static(__dirname + '/public'));
+
 /*********** When User Gets / The Index.HTML is Displayed with Static CSS and JS ***********/
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
+  res.sendFile(__dirname + '/indexTest.html')
   console.log('Arrived at Index Home')
 
 })
 
+/*Idea, send info to Database, set equal to key, pull info from database on form.on*/
+/*********** Formidable Node.JS Library to Import User Submitted Form & Save to Disk ***********/
+app.post('/upload', function (req, res){
+   /* Formidable Default Variable for Form Function */
+    var form = new formidable.IncomingForm();
+    /* Later Make This a Hash Key */
+    var key = Math.floor(Math.random() * Math.floor(9999999999999));
+    console.log(key);
+
+    /* Creating a Document (ROW) in MongoDB with a Key (ID) for the Customer for later Matching */
+    db.collection('userinfov2').insert({id: key});
+   
+/* Getting the req.body Info (Tracking Number, Email, OS) */
+    form.parse(req, function(err, fields, files, name, file) 
+          {
+            let custTrackingNumber2 = fields.trackingNumber;
+            let custEmail2 = fields.email;
+            let custOperatingSystem2 = fields.operatingSystem;
+            let custInfoArray2 = [custTrackingNumber2, custEmail2, custOperatingSystem2];
+            db.collection('userinfov2').update({id: key}, {$set: {"custEmail": custEmail2}});
+            db.collection('userinfov2').update({id: key}, {$set: {"custTrackingNumber": custTrackingNumber2}});
+            db.collection('userinfov2').update({id: key}, {$set: {"custOperatingSystem": custOperatingSystem2}});
+            console.log(custInfoArray2); 
+     });
+
+    /* Change Default Options | Sets the Path Name and Submits it 
+    /* fileBegin - Emitted whenever a new file is detected in the upload stream. 
+    /* Use this event if you want to stream the file to somewhere else while buffering the upload on the file system*/
+    form.on('fileBegin', function (name, file){
+      form.on('progress', function(bytesReceived, bytesExpected) {
+      console.log("test fileBegin", bytesReceived, bytesExpected)
+       });
+      file.path = __dirname + '/uploads/'+key+'.txt' ;   
+      console.log("File Path Set to: "+  file.path);
+    });
+
+
+    /* Submits Confirmation Message for the File to Defined Directory */
+    /* file command - Emitted whenever a field / file pair has been received. file is an instance of File. */
+    form.on('file', function (name, file){;
+        console.log('Uploaded ' + file.name); 
+      form.on('progress', function(bytesReceived, bytesExpected) {
+      console.log("test file", bytesReceived, bytesExpected)
+       });
+    });
+
+    /* Runs after File Transfer Finishes - fs.rename Renames the File*/
+    form.on('end', function() {
+        /* Grabs the Tracking Number w/ Use of the Generated User Identifier */
+        db.collection('userinfov2').find({id:key}, {projection: { _id: 0, custTrackingNumber: 1}})
+              .toArray((function(err, result){
+                   if (err) throw err;
+                   let custTrackingNumber = result[0].custTrackingNumber;
+                   console.log(custTrackingNumber);
+                   /* Renames the File with the Tracking Number */
+                   fs.rename('./uploads/'+key+'.txt', './uploads/'+custTrackingNumber+'.txt', function (err) {
+                      if (err) throw err;
+                      console.log('renamed complete');
+                  });
+         }));
+    });
+   
+    /* User needs a Post Response (Redirect to Home -> Bypass in Index) */
+    res.redirect('/');
+});
 
 
 // app.post('/informationRequest', (req, res) => {
@@ -94,24 +155,3 @@ app.get('/', (req, res) => {
 //     main().catch(console.error);
 //     /*******************************************************************************/
 // })
-
-
-app.post('/upload', function (req, res){
-    var form = new formidable.IncomingForm();
-    form.parse(req);
-
-    form.parse(req, function(err, fields, files) {
-    console.log(fields);
-    });
-
-    form.on('fileBegin', function (name, file){
-        file.path = __dirname + '/uploads/' + "test15.txt";
-    });
-
-    form.on('file', function (name, file){
-        console.log('Uploaded ' + file.name);
-    });
-    res.redirect('/')
-
-});
-
