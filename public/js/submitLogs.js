d785ab160907b706c9243d0f3b26a2b3b9228ff2
Kristoffer Stobbe <krisstobbe@yahoo.com>
2019-04-16 16:20:19 -0500

    Updating CSS - Fixing Element Display Sizes

--- public/js/submitLogs.js
+++ public/js/submitLogs.js
@@ -19,6 +19,32 @@ var form = document.getElementById("submitTestButton");
 /* Setting the Header and Body Hidden when the Site loads*/
 document.getElementById("testsdwcheck").hidden = false;
 document.getElementById("logColectorAssistance").hidden = false;
+document.getElementById("dummyframe").hidden = true;
+
+
+/*MODAL STUFF */
+// Get the modal
+var modal = document.getElementById('myModal');
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+/*********************/
+
 
 /* Length Function(s) - Finding the Length of an Input and Returning it */ /* Future Efficiency Increase- Implement this into an Array */
 function inputLength() {
@@ -32,7 +58,7 @@ function inputLengthTrackingNumber() {
 }
 
 function submitAlert(){
-	alert("Your Logs have been Submitted")
+	// alert("Your Logs have been Submitted")
 }
 
 /* Output Email and Tracking Number Values for Storage Purposes*/
